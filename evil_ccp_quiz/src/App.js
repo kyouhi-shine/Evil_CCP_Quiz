559b75210646234a2ab4ea8a689f391b4cefa0a6
kyouhi_shine <kyouhi_shine@proton.me>
2025-06-28 22:57:34 +0900

    大部分終わり

--- evil_ccp_quiz/src/App.js
+++ evil_ccp_quiz/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import questionsData from "./questions.json";
+import './style.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [questions, setQuestions] = useState([]);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [flag, setFlag] = useState(false);
+
+    useEffect(() => {
+        const data = questionsData.map(item => ({
+            ...item,
+            clicked: -1
+        }))
+        setQuestions(data);
+    }, []);
+
+    const handleAnswer = idx => {
+        if (flag) return;
+        const updatedQuestions = [...questions];
+        updatedQuestions[currentQuestion] = {
+            ...updatedQuestions[currentQuestion],
+            clicked: idx
+        };
+        setQuestions(updatedQuestions);
+
+        if (idx === questions[currentQuestion].answer) {
+            setFlag(true);
+            setScore(score + 1);
+            console.log(q.clicked)
+            setTimeout(() => {
+                nextQuestion();
+                setFlag(false);
+            }, 1000);
+        }
+    };
+    const nextQuestion = () => {
+        if (currentQuestion < questions.length - 1) setCurrentQuestion(prev => prev + 1);
+    }
+
+    if (questions.length === 0) return <div>Loading...</div>;
+    const q = questions[currentQuestion];
+
+    return (
+        <div className="App">
+            <div className="game-container">
+                <h1>支那のクイズ</h1>
+                <h2>得点：{score}</h2>
+                <p className="questions-title">{q.question}</p>
+                <div className="questions-answer-option">
+                    {q.options.map((opt, idx) => {
+                        let className = '';
+                        if (q.clicked === idx) {
+                            className = idx === q.answer ? 'correct' : 'wrong';
+                        }
+
+                        return (
+                            <button className={className}
+                                key={idx} onClick={() => handleAnswer(idx)}>{opt}</button>
+                        )
+                    })}
+                </div>
+                <div onClick={nextQuestion}>次へ</div>
+                <div>
+                    Question {currentQuestion + 1} of {questions.length}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
