4f37331ca6a62feef1f8739d9cb7a2931e0fd466
Privacy <privacy@MacBook-Air.local>
2025-07-03 20:13:41 +0900

    データはデーターベースから出来た

--- evil_ccp_quiz/src/App.js
+++ evil_ccp_quiz/src/App.js
@@ -1,34 +1,42 @@
 import React, { useEffect, useState } from "react";
-import questionsData from "./questions.json";
 import './style.css';
 
 function App() {
-    const [questions, setQuestions] = useState([]);
+    const [question, setQuestion] = useState({});
+    const [questionsLength, setQuestionsLength] = useState(0);
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [score, setScore] = useState(0);
     const [flag, setFlag] = useState(false);
 
     useEffect(() => {
-        const data = questionsData.map(item => ({
-            ...item,
-            clicked: -1
-        }))
-        setQuestions(data);
-    }, []);
+        fetch("https://shina-quiz-proxy.j5j2fgpfk7.workers.dev", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ id: currentQuestion + 1 })
+        }).then(res => res.json())
+            .then(data => {
+                console.log("shina-quiz: ", data);
+                setQuestionsLength(data.total)
+                setQuestion({
+                    ...data.data[0],
+                    clicked: -1
+                });
+            })
+    }, [currentQuestion]);
 
     const handleAnswer = idx => {
         if (flag) return;
-        const updatedQuestions = [...questions];
-        updatedQuestions[currentQuestion] = {
-            ...updatedQuestions[currentQuestion],
+        if (question.answer === question.clicked) return;
+        setQuestion({
+            ...question,
             clicked: idx
-        };
-        setQuestions(updatedQuestions);
+        })
 
-        if (idx === questions[currentQuestion].answer) {
+        if (idx === question.answer) {
             setFlag(true);
             setScore(score + 1);
-            console.log(q.clicked)
             setTimeout(() => {
                 nextQuestion();
                 setFlag(false);
@@ -36,40 +44,39 @@ function App() {
         }
     };
     const nextQuestion = () => {
-        if (currentQuestion < questions.length - 1) setCurrentQuestion(prev => prev + 1);
+        if (currentQuestion < questionsLength - 1) setCurrentQuestion(prev => prev + 1);
     }
 
-    if (questions.length === 0) return <div>Loading...</div>;
-    const q = questions[currentQuestion];
+    if (questionsLength === 0) return <div>Loading...</div>;
+
 
     return (
         <div className="App">
             <div className="game-container">
                 <h1>支那のクイズ</h1>
                 <h2>得点：{score}</h2>
-                <p className="questions-title">{q.question}</p>
+                <p className="questions-title">{question.question}</p>
                 <div className="questions-answer-option">
-                    {q.options.map((opt, idx) => {
+                    {question.options.map((opt, idx) => {
                         let className = '';
-                        if (q.clicked === idx) {
-                            className = idx === q.answer ? 'correct' : 'wrong';
+                        if (question.clicked === idx) {
+                            className = idx === question.answer ? 'correct' : 'wrong';
                         }
 
                         return (
-                            <button className={className}
-                                key={idx} onClick={() => handleAnswer(idx)}>{opt}</button>
+                            <button className={className} key={idx} onClick={() => handleAnswer(idx)}>{opt}</button>
                         )
                     })}
                 </div>
                 <div onClick={nextQuestion}>次へ</div>
                 <div>
-                    Question {currentQuestion + 1} of {questions.length}
+                    Question {currentQuestion + 1} of {questionsLength}
                 </div>
                 <br/>
-                {q.link &&
+                {question.link &&
                     <>
                         <span>出典：</span>
-                        <a href={q.link} target="_blank">{q.link_title}</a>
+                        <a href={question.link} target="_blank">{question.link_title}</a>
                     </>
                 }
             </div>
